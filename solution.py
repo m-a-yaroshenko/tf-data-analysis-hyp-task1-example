f50f51c86e4a06c37de6364173ac8f7eb451acc8
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-10 21:56:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +12,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    conv_control = x_success / x_cnt
+    conv_test = y_success / y_cnt
+    diff = conv_test - conv_control
+    se = ((1-conv_test)/y_cnt + (1-conv_control)/x_cnt)**0.5
+    t_stat = diff/se
+    p_value = 2*(1 - stats.t.cdf(abs(t_stat), x_cnt + y_cnt - 2))
+    alpha = 0.09
+    return True if p_value <= alpha else False
