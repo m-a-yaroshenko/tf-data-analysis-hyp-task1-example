45cbaa220b0017c8a14759ed4af478b5125e8213
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-11 09:16:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+import math
 
 
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
@@ -12,11 +12,9 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    conv_control = x_success / x_cnt
-    conv_test = y_success / y_cnt
-    diff = conv_test - conv_control
-    se = ((1-conv_test)/y_cnt + (1-conv_control)/x_cnt)**0.5
-    t_stat = diff/se
-    p_value = 2*(1 - stats.t.cdf(abs(t_stat), x_cnt + y_cnt - 2))
-    alpha = 0.09
-    return True if p_value <= alpha else False
+    p_c = x_success / x_cnt
+    p_t = y_success / y_cnt
+    SE = math.sqrt((p_t*(1-p_t))/y_cnt + (p_c*(1-p_c))/x_cnt)
+    z_stat = (p_t - p_c) / SE
+    z_crit = 1.645 # при alpha=0.09
+    return True if abs(z_stat) > z_crit else False
