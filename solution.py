5cc37f6a9966d92b78c9ee85ccc07c3898ce9c8e
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-13 18:12:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+from scipy.stats import norm
 
 
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -12,11 +14,10 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    conv_control = x_success / x_cnt
-    conv_test = y_success / y_cnt
-    diff = conv_test - conv_control
-    se = ((1-conv_test)/y_cnt + (1-conv_control)/x_cnt)**0.5
-    t_stat = diff/se
-    p_value = 2*(1 - stats.t.cdf(abs(t_stat), x_cnt + y_cnt - 2))
-    alpha = 0.09
-    return True if p_value <= alpha else False
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    SE = ((p1*(1-p1))/x_cnt + (p2*(1-p2))/y_cnt) ** 0.5
+    Z = (p2 - p1) / SE
+    Z_alpha = norm.ppf(1 - 0.09/2)
+    return True if Z > Z_alpha else False
+  
